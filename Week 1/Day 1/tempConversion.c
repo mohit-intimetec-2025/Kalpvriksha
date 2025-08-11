4ec76e4fc97bcbf8b834dfbb36be36a98953db6d
mohit-intimetec-2025 <mohit.choudhary@intimetec.com>
2025-08-11 17:18:29 +0530

    bug fixed

--- Week 1/Day 1/tempConversion.c
+++ Week 1/Day 1/tempConversion.c
@@ -10,7 +10,7 @@ void main(){
     // convert degree celcius to degree farhenite 
     // °F = (9/5 × °C) + 32.
 
-    float farhenite = (9/5.0 + temp) +32;
+    float farhenite = (9/5.0 * temp) +32;
 
     printf("The temperature in farhenite is : %.2f", farhenite);
 
