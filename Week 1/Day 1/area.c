b70761584a8be9328c7210970c41af29ad5fd8ad
mohit-intimetec-2025 <mohit.choudhary@intimetec.com>
2025-08-11 17:09:24 +0530

    Day 1 - understood the basics of C programming

---
+++ Week 1/Day 1/area.c
@@ -0,0 +1,18 @@
+// calculate area and perimeter of a circle
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int radius;
+    printf("Enter the Radius of the circle: ");
+    scanf("%d",&radius);
+    
+    float perimeter = 2* 3.14*radius;
+    // perimeter 
+    printf("The perimeter of the cirlce is %.2f\n",perimeter);
+
+    //area
+    float area = 3.14 * radius * radius;
+    printf("The area of the cirlce is %.2f\n",area);
+}
---
+++ Week 1/Day 1/tempConversion.c
@@ -0,0 +1,18 @@
+// Temperature conversion 
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    float temp;
+    printf("Enter the temperature in Celcius : ");
+    scanf("%.2f",&temp);
+
+    // convert degree celcius to degree farhenite 
+    // °F = (9/5 × °C) + 32.
+
+    float farhenite = (9/5.0 + temp) +32;
+
+    printf("The temperature in farhenite is : %.2f", farhenite);
+
+
+}
